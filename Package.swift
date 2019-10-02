b74b966310899d5920d2636cbe380e51d09ef98b
Alexander Martynenko <amarty01@gmail.com>
2019-10-03 00:23:59 +0300

    update package

--- Package.swift
+++ Package.swift
@@ -1,13 +1,10 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "wallet-kit",
-    platforms: [
-        .macOS(.v10_12)
-    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
